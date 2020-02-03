9ad960758c8238077e012598363b552227d0239a
amitbhudiya <59469748+amitbhudiya@users.noreply.github.com>
2020-02-03 20:14:40 +0530

    Update HAMCODE.C

--- HAMCODE.C
+++ HAMCODE.C
@@ -49,86 +49,16 @@ d=(p1*1)+(p2*2)+(p3*4);
 
 printf("\nError at position %d\n",d);
 
-if(d==7)
-  {
-   if(code[1]==1)
-    {code[1]=0;}
+if(code[d-7]==1)
+{
+  code[d-7]=0;
+}
   else
-   {code[1]=1;}
-  }
-
-if(d==6)
   {
-   if(code[2]==1)
-  {
-   code[2]=0;
-   }
-   else{
-     code[2]=1;
+    code[d-7]=1;
   }
-  }
-
-
-if(d==5)
-{
- if(code[3]==1)
- {
-code[3]=0;
-}
-else{
-code[3]=1;
-}
-}
-
-if(d==4)
-{
-if(code[4]==1)
-{
-code[4]=0;
-}
-else{
-code[4]=1;
-}
-}
-
-
-if(d==3)
-{
-if(code[5]==1)
-{
-code[5]=0;
-}
-else{
-code[5]=1;
-}
-}
-
-
-if(d==2)
-{
-if(code[6]==1)
-{
-code[6]=0;
-}
-else{
-code[6]=1;
-}
-}
-
-
-if(d==1)
-{
-if(code[7]==1)
-{
-code[7]=0;
-}
-else{
-code[7]=1;
-}
-}
-
 i=0;
-for(i=1;i<8;i++){
+for(i=0;i<7;i++){
 printf(" %d ",code[i]);
 }
 getch();
